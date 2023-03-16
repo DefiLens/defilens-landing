e80a9a51872161512c902cc35797af34f637a8d9
sunnyRK <radadiyasunny970@gmail.com>
2023-03-16 12:21:30 +0530

    added info about protocol

--- modules/landing/SocialGraph/SocialGraph.tsx
+++ modules/landing/SocialGraph/SocialGraph.tsx
@@ -3,17 +3,17 @@ const SocialGraph = () => (
     className="p-[10px] text-white h-full flex flex-col items-center text-center"
     style={styles.container}
   >
+      <div className="pt-[30px] text-[20px]" style={{color: "red"}}>
+        Currently on mumbai network. You should have lens profile on mumbai network.
+        soon you can create lens profile for mumbai network. so you can test.
+        By 25th March, We will live stable beta version of defilens. Stay tune :)
+      </div>
     <div className="flex justify-center flex-col items-center pt-[100px] w-[800px]">
       <div className="text-[60px] leading-[60px]">
         The Social-Graph for Defi builds as it Discuss and Social Trades.
       </div>
       <div className="pt-[60px] text-[22px]">
         DefiLens is a Social graph for Defi. Which enable users to create social trade on defi and defi activity on lens and give features to discuss on top of it in a interactive way.
-      </div>
-      <div className="pt-[30px] text-[15px]">
-        Currently on mumbai network. You should have lens profile on mumbai network.
-        soon you can create lens profile for mumbai network. so one can test
-        By 25th March, Will live stable beta version of defilens.
       </div>
       <a href="https://forms.gle/N4Gy1x97ZTVvx32F8" target="_blank">
         <button className='bg-black text-white text-bold h-[50px] rounded-[8px] w-[225px] mt-[60px]'>
