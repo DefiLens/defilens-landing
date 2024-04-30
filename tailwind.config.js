435b7b551832f085f332ea3a6ddeb7e43a812b84
Sarthak Verma <sarthakvdev@gmail.com>
2024-04-30 14:32:13 +0530

    bumped deps, updated export config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-// const withMT = require("@material-tailwind/react/utils/withMT");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
@@ -12,9 +10,6 @@ module.exports = {
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
-
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
