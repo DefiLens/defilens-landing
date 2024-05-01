ef8738ae8ac4729ad0ff59f9ee82ed26a937b865
Sarthak Verma <sarthakvdev@gmail.com>
2024-05-01 13:08:58 +0530

    readme updated + utils revamp

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
-import { BASE_URL } from '../utils/constants';
+import { BASE_URL } from './constants';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
