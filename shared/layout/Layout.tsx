b7688fa47700c96b6b002ede7d4c999bccd63b79
Sarthak Verma <sarthakvdev@gmail.com>
2024-05-01 13:37:57 +0530

    fonts: inter and satoshi optimised

--- shared/layout/Layout.tsx
+++ shared/layout/Layout.tsx
@@ -1,8 +1,15 @@
+import clsx from "clsx";
+import { satoshiFont } from "@/utils/fonts";
 import { tLayout } from "./types/index";
 
 const Layout = ({ children }: tLayout) => {
   return (
-    <div>
+    <div
+      className={clsx(
+        "font-inter",
+        satoshiFont.variable
+      )}
+    >
       {children}
     </div>
   );
