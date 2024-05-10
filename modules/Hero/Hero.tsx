ea18276106170906cf89e5d0f137bf3e4bbff131
Sarthak Verma <sarthakvdev@gmail.com>
2024-05-10 22:07:08 +0530

    responsiveness

--- modules/Hero/Hero.tsx
+++ modules/Hero/Hero.tsx
@@ -18,8 +18,8 @@ const Hero = () => {
     <div
       className={clsx(
         "hero-container flex flex-col justify-start items-center text-center gap-4 lg:gap-6",
-        "h-screen 2xl:gap-32 text-primary-1000 bg-primary-100",
-        "p-8 sm:p-20 md:p-32 lg:p-24 pt-40 sm:pt-40 md:pt-40 lg:pt-44 2xl:pt-48"
+        "sm:h-screen 2xl:gap-32 text-primary-1000 bg-primary-100",
+        "p-8 sm:p-20 lg:p-24 pt-32 sm:pt-40 md:pt-40 lg:pt-44 2xl:pt-48"
       )}
     >
       <div className="bg-transparent max-w-[1380px] w-full md:w-[94%] h-full flex flex-col lg:flex-row justify-start lg:justify-center items-center text-center gap-8 lg:gap-16 2xl:gap-28 text-primary-1000 bg-primary-100">
@@ -27,22 +27,23 @@ const Hero = () => {
         <div
           className="h-full flex flex-col justify-center items-start gap-5"
           ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateY(-64px)",
-            opacity: isInView ? 1 : 0,
-            transition:
-              "transform: none;opacity: 0;transition: all 1.0s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0.3s;",
-          }}
+          // style={{
+          //   transform: isInView ? "none" : "translateY(-64px)",
+          //   opacity: isInView ? 1 : 0,
+          //   transition:
+          //     "transform: none;opacity: 0;transition: all 1.0s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0.3s;",
+          // }}
         >
           {/* Heading and Description */}
-          <h1 className="text-start text-xl md:text-3xl lg:text-7xl font-bold font-satoshi">
+          <h1 className=" text-center lg:text-start text-4xl md:text-5xl lg:text-7xl font-bold font-satoshi">
             <span className="mr-3">Unified Defi trading experience with</span>
             <span className="inline-flex text-primary-700">One-click</span>
           </h1>
-          <h6 className="text-start text-primary-300 text-xs md:text-sm lg:text-xl leading-8 font-semibold">
+          <h6 className=" text-center sm:text-start text-primary-300 text-xs md:text-sm lg:text-xl leading-5 sm:leading-8 font-semibold">
             DefiLens abstract chains and execute multiple orders in batch
             across-chains with smart wallet designed platform.
           </h6>
+          {/* Supported by */}
           <div className="mt-8 inline-flex items-center justify-between w-full">
             {/* Supported By */}
             <div className="flex flex-row items-center gap-3 md:gap-3 text-sm md:text-base font-semibold font-satoshi">
@@ -76,25 +77,25 @@ const Hero = () => {
           alt="hero-img"
           className="w-auto lg:w-[350px] xl:w-[450px] 2xl:w-[575px] 3xl:w-[750px]"
           ref={ref}
-          style={{
-            transform: isInView ? "none" : "translateY(-64px)",
-            opacity: isInView ? 1 : 0,
-            transition:
-              "transform: none;opacity: 0;transition: all 1.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0.3s;",
-          }}
+          // style={{
+          //   transform: isInView ? "none" : "translateY(-64px)",
+          //   opacity: isInView ? 1 : 0,
+          //   transition:
+          //     "transform: none;opacity: 0;transition: all 1.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0.3s;",
+          // }}
         />
       </div>
 
       {/* Supported Networks */}
       <div
-        className="w-full h-full flex flex-wrap justify-center items-center gap-5 lg:gap-8 mt-20 select-none"
+        className="w-full h-full grid grid-cols-3 sm:flex flex-wrap justify-center items-center gap-5 lg:gap-8 mt-5 lg:mt-20 select-none"
         ref={ref}
-        style={{
-          transform: isInView ? "none" : "translateY(-64px)",
-          opacity: isInView ? 1 : 0,
-          transition:
-            "transform: none;opacity: 0;transition: all 1.8s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0.3s;",
-        }}
+        // style={{
+        //   transform: isInView ? "none" : "translateY(-64px)",
+        //   opacity: isInView ? 1 : 0,
+        //   transition:
+        //     "transform: none;opacity: 0;transition: all 1.8s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0.3s;",
+        // }}
       >
         {BlockchainList.length > 0 &&
           BlockchainList.map((item) => (
