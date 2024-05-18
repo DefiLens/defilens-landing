a72f9b19bf40218f52df77d26c33aaa4719d7c2a
01Akshayjangra <akshayjangra225@gmail.com>
2024-05-18 15:32:51 +0530

    Added MS Clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google analytics tag */}
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
@@ -18,6 +19,19 @@ export default function Document() {
               page_path: window.location.pathname,
               });
               `,
+          }}
+        />
+
+        {/* Clarity tag for analytics  */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                      (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                      })(window, document, "clarity", "script", "m4qc0uxgik");
+                    `,
           }}
         />
       </Head>
