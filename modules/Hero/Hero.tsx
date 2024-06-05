bcc860d244e77c048d73c959b81fd28cfb3af23c
01Akshayjangra <akshayjangra225@gmail.com>
2024-06-05 11:20:07 +0530

    Fixed Ui + Responsiveness

--- modules/Hero/Hero.tsx
+++ modules/Hero/Hero.tsx
@@ -32,14 +32,15 @@ const Hero = () => {
   return (
     <div
       className={clsx(
-        "hero-container flex flex-col justify-start items-center text-center gap-4 lg:gap-6",
-        "sm:min-h-screen 2xl:gap-32 text-primary-1000 bg-primary-100",
-        "p-8 sm:p-20 lg:p-24 pt-32 sm:pt-40 md:pt-40 lg:pt-44 2xl:pt-48"
+        "hero-container flex flex-col justify-start items-center text-center",
+        "sm:min-h-screen gap-4 lg:gap-6 xl:gap-10 text-primary-1000 bg-primary-100",
+        "p-8 sm:p-10 lg:p-12",
+        "pt-32 sm:pt-40 md:pt-40 lg:pt-44 2xl:pt-48"
       )}
     >
-      <div className="bg-transparent max-w-[1380px] w-full md:w-[94%] h-full flex flex-col lg:flex-row justify-start lg:justify-center items-center text-center gap-8 lg:gap-16 2xl:gap-28 text-primary-1000 bg-primary-100">
+      <div className="bg-transparent max-w-[1380px] w-full md:w-[94%] h-full flex flex-col lg:flex-row justify-start lg:justify-center items-center text-center gap-8 lg:gap-10 text-primary-1000 bg-primary-100">
         {/* Left Content */}
-        <div className="w-1/2 h-full flex flex-col justify-center items-start gap-5">
+        <div className="flex-1 h-full flex flex-col justify-center items-center lg:items-start gap-5 lg:gap-10">
           {/* Heading and Description */}
           <motion.h1
             initial={{ opacity: 0, scale: 0.5 }}
@@ -49,17 +50,13 @@ const Hero = () => {
               delay: 0.2,
               ease: [0, 0.71, 0.2, 1.01],
             }}
-            className="text-center lg:text-start text-4xl md:text-5xl lg:text-7xl font-bold font-satoshi"
+            className="text-center lg:text-start text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-bold font-satoshi"
           >
-            <div className="font-bold text-primary-800 mb-4 leading-[1.2]">
-              {/* <span>{headings[currentHeading]}</span>
-              {(currentHeading === 1 || currentHeading === 2) && (
-                <span className="inline-flex text-primary-700 ml-2">
-                  One-click
-                </span>
-              )} */}
+            <div className="font-bold text-primary-800 leading-[1.2]">
               <span>Rebalance or Migrate portfolio with</span>
-              <span className="inline-flex text-primary-700 ml-2">One-click</span>
+              <span className="inline-flex text-primary-700 ml-2">
+                One-click
+              </span>
             </div>
           </motion.h1>
           <motion.h6
@@ -70,11 +67,22 @@ const Hero = () => {
               delay: 0.2,
               ease: [0, 0.71, 0.2, 1.01],
             }}
-            className="text-center sm:text-start text-primary-850 text-xs md:text-sm lg:text-xl leading-5 sm:leading-8 font-medium"
+            className="w-3/4 lg:w-full text-center lg:text-start text-primary-850 text-base md:text-lg lg:text-xls sm:leading-8 font-medium"
           >
             DefiLens abstract chains and execute multiple orders in batch
             across-chains with smart wallet designed platform.
           </motion.h6>
+          <div className="lg:hidden transition-all text-xs md:text-base font-semibold bg-primary-700 hover:scale-105 rounded-full shadow-md hover:shadow-2xl p-0.5">
+            <button className="flex h-full w-full items-center justify-center rounded-full">
+              <Link
+                href={meta.app}
+                target="_blank"
+                className="flex justify-center items-center gap-1 rounded-full text-white py-2 px-4 sm:px-7"
+              >
+                {buttonsText.heroCta}
+              </Link>
+            </button>
+          </div>
           {/* Supported by */}
           <motion.div
             initial={{ opacity: 0, scale: 0.5 }}
@@ -84,30 +92,19 @@ const Hero = () => {
               delay: 0.4,
               ease: [0, 0.71, 0.2, 1.01],
             }}
-            className="mt-8 inline-flex items-center justify-between w-full"
+            className="mt-0 lg:mt-2 inline-flex items-center justify-center lg:justify-between w-full"
           >
             {/* Supported By */}
             <div className="flex flex-row items-center gap-3 md:gap-3 text-sm md:text-base font-semibold font-satoshi">
               <h1 className="text-center text-xl md:text-2xl font-bold !leading-tight">
-                Grant Received<span className="px-1 text-primary-700">from</span>
+                Grant Received
+                <span className="px-2 text-primary-700">from</span>
               </h1>
               {backendIcons.map((item: string, index) => (
                 <div key={index} className="w-6 h-6 md:w-8 md:h-8">
                   <Image src={item} className="w-full h-full" alt="icon" />
                 </div>
               ))}
-            </div>
-            {/* Launch App Button */}
-            <div className="transition-all text-xs md:text-base font-semibold bg-primary-700 hover:scale-105 rounded-full shadow-md hover:shadow-2xl p-0.5">
-              <button className="flex h-full w-full items-center justify-center rounded-full">
-                <Link
-                  href={meta.app}
-                  target="_blank"
-                  className="flex justify-center items-center gap-1 rounded-full text-white py-2 px-4 sm:px-7"
-                >
-                  {buttonsText.heroCta}
-                </Link>
-              </button>
             </div>
           </motion.div>
         </div>
@@ -120,18 +117,31 @@ const Hero = () => {
             delay: 0.7,
             ease: [0, 0.71, 0.2, 1.01],
           }}
-          className="w-1/2 h-full"
+          className="flex-1 h-full"
         >
           <Image
             src={hero}
             alt="hero-img"
-            className="w-auto lg:w-[350px] xl:w-[450px] 2xl:w-[575px] 3xl:w-[750px]"
+            className="mx-auto w-[350px] md:w-[350px] xl:w-[450px] 2xl:w-[575px] 3xl:w-[750px]"
           />
         </motion.div>
       </div>
 
+      {/* Launch App Button */}
+      <div className="hidden lg:block transition-all text-xs md:text-base font-semibold bg-primary-700 hover:scale-105 rounded-full shadow-md hover:shadow-2xl p-0.5">
+        <button className="flex h-full w-full items-center justify-center rounded-full">
+          <Link
+            href={meta.app}
+            target="_blank"
+            className="flex justify-center items-center gap-1 rounded-full text-white py-2 px-4 sm:px-7"
+          >
+            {buttonsText.heroCta}
+          </Link>
+        </button>
+      </div>
+
       {/* Supported Networks */}
-      <div className="w-full h-full grid grid-cols-3 sm:flex flex-wrap justify-center items-center gap-5 lg:gap-8 mt-5 lg:mt-20 select-none">
+      <div className="mt-5 w-full h-full grid grid-cols-3 sm:flex flex-wrap justify-center items-center gap-5 lg:gap-8 select-none">
         {BlockchainList.length > 0 &&
           BlockchainList.map((item) => (
             <div key={item.key} className="flex items-center gap-3">
